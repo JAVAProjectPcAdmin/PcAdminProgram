405ffc3dfe8ab45d26b06c14af55240f34141f4c
Kimjonghoon <ilms9012@gmail.com>
2017-09-04 17:38:26 +0900

    요금정보 껍데기 완성
    완성

--- src/userUsingState/ChargeInformationGUI.java
+++ src/userUsingState/ChargeInformationGUI.java
@@ -15,21 +15,23 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class ChargeInformationGUI extends JFrame implements ActionListener {
 
-	private JLabel titleLabel, nameLabel,rs_nameLabel, startLabel,rs_startLabel, usingTimeLabel,rs_usingTimeLabel, chargeLabel,rs_chargeLabel, orderChargeLabel,rs_orderChargeLabel, sumLabel,rs_sumLabel;
-	private JPanel panel, moenyBagIcon;
+	private JLabel titleLabel, nameLabel, rs_nameLabel, startLabel, rs_startLabel, usingTimeLabel, rs_usingTimeLabel,
+			chargeLabel, rs_chargeLabel, orderChargeLabel, rs_orderChargeLabel, sumLabel, rs_sumLabel;
+	private JPanel panel, moneyBagIcon;
+	private JButton exitButton, detailButton;
 
 	BufferedImage moneybagImg = null;
 
 	public ChargeInformationGUI() {
 		setTitle("¿ä±ÝÁ¤º¸");
 		setLayout(null);
-		setLocation(400, 300);
-		setSize(310, 400);
+		setLocation(950, 330);
+		setSize(310, 370);
 		setResizable(false);
+		setUndecorated(true);
 		setAlwaysOnTop(true);
 
 		try {
@@ -45,7 +47,15 @@ public class ChargeInformationGUI extends JFrame implements ActionListener {
 		panel.setLayout(null);
 		panel.setSize(400, 420);
 
-		moenyBagIcon = new moenyBagIcon();
+		exitButton = new JButton("È®ÀÎ");
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose(); // Ã¢ÇÏ³ª¸¸ Á¾·á ÀüÃ¼Á¾·á´Â system.exit
+			}
+		});
+
+		detailButton = new JButton(new ImageIcon("detailinformation.png"));
+		moneyBagIcon = new moenyBagIcon();
 		titleLabel = new JLabel("¿ä±ÝÁ¤º¸");
 		titleLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 30));
 		nameLabel = new JLabel(" * ÀÌ¸§");
@@ -60,47 +70,56 @@ public class ChargeInformationGUI extends JFrame implements ActionListener {
 		orderChargeLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
 		sumLabel = new JLabel(" * ÇÕ°è");
 		sumLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 16));
-////////////////////////////////////////////////////////////////////////////////////		
-		rs_nameLabel = new JLabel(" * ÀÌ¸§");
+		////////////////////////////////////////////////////////////////////////////////////
+		rs_nameLabel = new JLabel("±èÁ¾ÈÆ");
 		rs_nameLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
-		rs_startLabel = new JLabel(" * ½ÃÀÛ½Ã°£");
+		rs_startLabel = new JLabel("2017-09-04 4:58");
 		rs_startLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
-		rs_usingTimeLabel = new JLabel(" * »ç¿ë ½Ã°£");
+		rs_usingTimeLabel = new JLabel("10:34");
 		rs_usingTimeLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
-		rs_chargeLabel = new JLabel(" * ½Ã°£ »ç¿ë ¿ä±Ý");
+		rs_chargeLabel = new JLabel("10900¿ø");
 		rs_chargeLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
-		rs_orderChargeLabel = new JLabel(" * À½½ÄÁÖ¹®±Ý¾×");
+		rs_orderChargeLabel = new JLabel("40100¿ø");
 		rs_orderChargeLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 12));
-		rs_sumLabel = new JLabel(" * ÇÕ°è");
+		rs_sumLabel = new JLabel("50000¿ø");
 		rs_sumLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 16));
-/////////////////////////////////////////////////////////////////////////////////////
-		moenyBagIcon.setBounds(30, 10, 64, 64);
-
+		/////////////////////////////////////////////////////////////////////////////////////
+		exitButton.setBounds(190, 330, 100, 20);
+		moneyBagIcon.setBounds(30, 10, 64, 64);
 		titleLabel.setBounds(120, 13, 200, 50);
 		nameLabel.setBounds(20, 110, 50, 50);
 		startLabel.setBounds(20, 140, 100, 50);
 		usingTimeLabel.setBounds(20, 170, 100, 50);
 		chargeLabel.setBounds(20, 200, 200, 50);
 		orderChargeLabel.setBounds(20, 230, 200, 50);
+		detailButton.setBounds(30, 268, 73, 23);
 		sumLabel.setBounds(20, 280, 80, 50);
-///////////////////////////////////////////////////////////////
-		titleLabel.setBounds(120, 13, 200, 50);
-		rs_nameLabel.setBounds(20, 110, 50, 50);
-		rs_startLabel.setBounds(20, 140, 100, 50);
-		rs_usingTimeLabel.setBounds(20, 170, 100, 50);
-		rs_chargeLabel.setBounds(20, 200, 200, 50);
-		rs_orderChargeLabel.setBounds(20, 230, 200, 50);
-		rs_sumLabel.setBounds(20, 280, 80, 50);
-////////////////////////////////////////////////////////////////		
+		///////////////////////////////////////////////////////////////
+
+		rs_nameLabel.setBounds(250, 110, 50, 50);
+		rs_startLabel.setBounds(191, 140, 100, 50);
+		rs_usingTimeLabel.setBounds(255, 170, 100, 50);
+		rs_chargeLabel.setBounds(240, 200, 200, 50);
+		rs_orderChargeLabel.setBounds(240, 230, 200, 50);
+		rs_sumLabel.setBounds(220, 280, 80, 50);
+		////////////////////////////////////////////////////////////////
 		panel.add(nameLabel);
 		panel.add(startLabel);
-		panel.add(moenyBagIcon);
+		panel.add(moneyBagIcon);
 		panel.add(titleLabel);
 		panel.add(usingTimeLabel);
 		panel.add(chargeLabel);
 		panel.add(orderChargeLabel);
 		panel.add(sumLabel);
-
+		//////////////////////////////////////////////////////////////
+		panel.add(rs_nameLabel);
+		panel.add(rs_startLabel);
+		panel.add(rs_usingTimeLabel);
+		panel.add(rs_chargeLabel);
+		panel.add(rs_orderChargeLabel);
+		panel.add(rs_sumLabel);
+		panel.add(exitButton);
+		panel.add(detailButton);
 		add(panel);
 
 		setVisible(true);
@@ -117,9 +136,5 @@ public class ChargeInformationGUI extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 
-	}
-
-	public static void main(String[] args) {
-		new ChargeInformationGUI();
 	}
 }
