c8fe6804191b7bf02a40084d8ba5c4bedeca00e1
jaein <jaein07227@gmail.com>
2017-09-04 17:50:28 +0900

    회원가입 완료
    ㄴㅇㄹㅇㄴㄹㅇㄹㄴ

--- src/db/UserDao.java
+++ src/db/UserDao.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 
 public class UserDao {
 	PreparedStatement pstnt = null;
@@ -30,23 +31,89 @@ public class UserDao {
 		} 
 	}
 	public void UserJoinInsert(UserVO user) {
+		Date date= new Date();
 		
-		sql= "INSERT INTO USER(ID,PASSWORD,NAME,REGISTER_NUMBER,PHONE,EMAIL_ADDRESS) "
-				+ " VALUES(?,?,?,?,?,?)";
+		sql= "INSERT INTO USER(ID,PASSWORD,NAME,RESIDENT_NUMBER,PHONE,EMAIL_ADDRESS,ADDRESS,REGISTER_DATE) "
+				+ " VALUES(?,?,?,?,?,?,?,?)";
 		
 		try {
 			con = DriverManager.getConnection(DB_URL,DB_ID ,DB_PW);
 			pstnt = con.prepareStatement(sql);
 			
+			pstnt.setString(1, user.getId());
+			pstnt.setString(2, user.getPassword());
+			pstnt.setString(3, user.getName());
+			pstnt.setString(4, user.getResidentNumber());
+			pstnt.setString(5, user.getPhone());
+			pstnt.setString(6, user.getEmailAddress());
+			pstnt.setString(7, user.getAddress());
+			pstnt.setString(8, date.toString());
+			
+			result = pstnt.executeUpdate();
+			System.out.println("insert : "+result);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}finally {
+			this.close();
 		}
+	}
+	public int UserIdSelect(String Id) {
+		int num=-1;
+		sql = "SELECT USER_NUMBER FROM USER WHERE ID=?";
+		
+		try {
+			con = DriverManager.getConnection(DB_URL,DB_ID,DB_PW);
+			pstnt=con.prepareStatement(sql);
+			
+			pstnt.setString(1, Id);
+			
+			rs=pstnt.executeQuery();
+			if(rs.next()) {
+				num=rs.getInt(1);
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			close();
+			closeRS();
+		}
+		return num;
+	
 	}
 	
 	
-	
+	public void close() {
+		if (pstnt != null) {
+			try {
+				pstnt.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				this.close();
+				e.printStackTrace();
+			}
+		}
+	}
+	public void closeRS() {
+		if(rs!=null)
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
 	
 	
 }
--- src/db/UserVO.java
+++ src/db/UserVO.java
@@ -8,8 +8,7 @@ public class UserVO {
 	private String password;
 	private String name;
 	private String residentNumber;
-	private String phone1;
-	private String phone2;
+	private String phone;
 	private String emailAddress;
 	private String Address;
 	private String registerDate;	
@@ -45,17 +44,11 @@ public class UserVO {
 	public void setResidentNumber(String residentNumber) {
 		this.residentNumber = residentNumber;
 	}
-	public String getPhone1() {
-		return phone1;
+	public String getPhone() {
+		return phone;
 	}
-	public void setPhone1(String phone1) {
-		this.phone1 = phone1;
-	}
-	public String getPhone2() {
-		return phone2;
-	}
-	public void setPhone2(String phone2) {
-		this.phone2 = phone2;
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 	public String getEmailAddress() {
 		return emailAddress;
