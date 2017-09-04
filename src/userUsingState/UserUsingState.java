0c6ba2c7cd85541f7f0a147cbee9b47fdf5558fa
Kimjonghoon <ilms9012@gmail.com>
2017-09-04 10:48:47 +0900

    유저사용현황 gui끝
    끝

--- src/userUsingState/UserUsingState.java
+++ src/userUsingState/UserUsingState.java
@@ -27,7 +27,7 @@ public class UserUsingState extends JFrame {
 		setSize(310, 280);
 		setResizable(false);
 		setUndecorated(true); // À©µµ¿ìÃ¢ À­ºÎºÐ Á¦°Å ¸Þ¼Òµå
-
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setAlwaysOnTop(true);
 		getContentPane().setBackground(Color.WHITE);
 
@@ -47,26 +47,40 @@ public class UserUsingState extends JFrame {
 		timeLb = new JLabel("00:00");
 		moneyLb = new JLabel("0¿ø");
 		talkLb = new JLabel("¹®ÀÇ");
-		talkLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 10));
+		talkLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 13));
 		orderLb = new JLabel("ÁÖ¹®");
-		orderLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 10));
+		orderLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 13));
 		informationLb = new JLabel("¿ä±ÝÁ¤º¸");
-		informationLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 10));
+		informationLb.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 13));
 
 		talkBt = new JButton(new ImageIcon("talk.png"));
+		talkBt.setBorderPainted(false);
 		orderBt = new JButton(new ImageIcon("order.png"));
+		orderBt.setBorderPainted(false);
 		informationBt = new JButton(new ImageIcon("information.png"));
+		informationBt.setBorderPainted(false);
 
 		panel.setBounds(10, 80, 283, 84);
 		customerNumberLb.setBounds(40, 0, 100, 100);
-		talkBt.setBounds(20, 100, 30, 30);
+		talkBt.setBounds(50, 180, 44, 32);
+		orderBt.setBounds(130, 180, 38, 29);
+		informationBt.setBounds(210, 180, 42, 36);
+		talkLb.setBounds(55, 210, 50, 50);
+		orderLb.setBounds(137, 210, 50, 50);
+		informationLb.setBounds(208, 210, 70, 50);
 
 		panel.add(timeLb);
 		panel.add(moneyLb);
 
 		timeLb.setBounds(90, 10, 70, 30);
 		moneyLb.setBounds(90, 45, 700, 30);
+
 		add(talkBt);
+		add(orderBt);
+		add(informationBt);
+		add(talkLb);
+		add(orderLb);
+		add(informationLb);
 		add(customerNumberLb);
 		add(panel);
 
