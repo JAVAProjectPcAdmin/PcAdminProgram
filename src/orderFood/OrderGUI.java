b7eaa25e875e145e0d5e132c8ca8f814f087be70
smile4994 <30884887+smile4994@users.noreply.github.com>
2017-09-04 16:16:12 +0900

    up
    up

--- src/orderFood/OrderGUI.java
+++ src/orderFood/OrderGUI.java
@@ -1,15 +1,13 @@
 package orderFood;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
+import java.awt.*;
 
-import javax.swing.*;
-
-import com.sun.prism.Graphics;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class OrderGUI extends JFrame {
 	private JPanel ramenPnl, drinkPnl, snackPnl, selectPnl, menuPnl, labelPnl;
@@ -23,9 +21,7 @@ public class OrderGUI extends JFrame {
 		snackPnl = new JPanel();
 		selectPnl = new JPanel();
 		menuPnl = new JPanel();
-
-		orderBtn = new JButton("ÁÖ¹®");
-		cancleBtn = new JButton("Ãë¼Ò");
+		labelPnl = new JPanel();
 
 		// ¸Þ´ºÅÇ
 		menuTab = new JTabbedPane();
@@ -33,14 +29,25 @@ public class OrderGUI extends JFrame {
 		menuTab.addTab("   À½   ·á   ¼ö   ", drinkPnl);
 		menuTab.addTab("   ½º   ³¼   ·ù   ", snackPnl);
 		menuTab.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 30));
-		// menuTab.setTabPlacement(JTabbedPane.LEFT);
+		 menuTab.setTabPlacement(JTabbedPane.LEFT);
 
 		menuPnl.add(menuTab);
-		menuPnl.setBounds(0, 0, 1000, 600);
+		// menuPnl.setBounds(0, 0, 1000, 600);
 		menuPnl.setBackground(Color.WHITE);
 		menuPnl.setLayout(new FlowLayout());
 
 		// menuPnl.setPreferredSize(new Dimension(400, 300));
+
+		/// ÃÑ »ç¿ë±Ý¾×
+		priceLabel = new JLabel("ÃÑ ±Ý¾× : ");
+		priceLabel.setFont(new Font("¸¼Àº °íµñ", Font.BOLD, 20));
+
+		labelPnl.add(priceLabel);
+		labelPnl.setBackground(Color.yellow);
+		labelPnl.setPreferredSize(new Dimension(0, -400));
+		// labelPnl.setBounds(0, 400, 1000, 100);
+
+		/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		// °áÁ¦, Ãë¼Ò
 		orderBtn = new JButton(" ÁÖ ¹® ");
@@ -52,20 +59,14 @@ public class OrderGUI extends JFrame {
 		orderBtn.setFocusPainted(false);
 		cancleBtn.setFocusPainted(false);
 
-		// selectPnl.setPreferredSize(new Dimension(0, -300));
+		selectPnl.setPreferredSize(new Dimension(0, -350));
 		selectPnl.setBackground(Color.RED);
 		// selectPnl.add(priceLabel);
 		selectPnl.add(orderBtn);
 		selectPnl.add(cancleBtn);
-		selectPnl.setBounds(0, 500, 1000, 400);
+		// selectPnl.setBounds(0, 500, 1000, 400);
+
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		/// ÃÑ »ç¿ë±Ý¾×
-		labelPnl = new JPanel();
-		priceLabel = new JLabel("ÃÑ ±Ý¾× : ");
-		
-		labelPnl.add(priceLabel);
-		labelPnl.setBounds(0, 400, 1000, 100);
-		
 
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -73,10 +74,10 @@ public class OrderGUI extends JFrame {
 		add(labelPnl);
 		add(selectPnl);
 
-		// setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
 		// setLayout(null);
 		setSize(1000, 800);
-		setResizable(false);
+		// setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setBackground(Color.WHITE);
 		// setUndecorated(true);
@@ -85,6 +86,6 @@ public class OrderGUI extends JFrame {
 	}
 
 	public static void main(String[] args) {
-		OrderGUI o = new OrderGUI();
+		OrderGUI oderGui = new OrderGUI();
 	}
 }
