da02dbf8b5c4c1369d1735fb71f47fa0bb2c1995
smile4994 <30884887+smile4994@users.noreply.github.com>
2017-09-05 15:18:30 +0900

    up
    up

--- src/adminMain/AdminMainGUI.java
+++ src/adminMain/AdminMainGUI.java
@@ -1,23 +1,30 @@
 package adminMain;
 
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 
 public class AdminMainGUI extends JFrame {
 	private JPopupMenu popup;
+	private LeftMainGUI lmp = new LeftMainGUI(); //
+	private RightMainGUI[] rightUserPanel = new RightMainGUI[25]; //
+
 	public AdminMainGUI() {
-		LeftMainGUI lmp = new LeftMainGUI();
-		RightMainGUI[] rightUserPanel=new RightMainGUI[25];
-		JPanel rightPanel= new JPanel();
+
+		JPanel rightPanel = new JPanel();
 
 		lmp.setBounds(0, 80, 220, 850);
 
@@ -25,25 +32,23 @@ public class AdminMainGUI extends JFrame {
 
 		for (int i = 0; i < 25; i++) {
 			rightUserPanel[i] = new RightMainGUI();
-			rightUserPanel[i].setSize(210,170);
+			rightUserPanel[i].setSize(210, 170);
 			rightPanel.add(rightUserPanel[i]);
 			rightPanel.addMouseListener(new PopupListener());
 		}
-		rightPanel.setBounds(230,90,1030,800);
+		rightPanel.setBounds(230, 90, 1030, 800);
+		lmp.getFindSeatBtn().addActionListener(new FindSeatActionListener());
 		add(rightPanel);
-		
-		
+
 		add(lmp);
 		setResizable(false);
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-		 popup = new JPopupMenu();
+		///////////////////////////////////////////////////////////////////////////////////////////////////////
+		popup = new JPopupMenu();
 		JMenuItem menuChat = new JMenuItem("대화걸기");
 		JMenuItem menuLogout = new JMenuItem("로그아웃");
 		popup.add(menuChat);
 		popup.add(menuLogout);
-//		menuChat.addActionListener(this);
-		
-		
+		// menuChat.addActionListener(this);
 
 		setLayout(null);
 		setSize(1280, 924);
@@ -53,25 +58,58 @@ public class AdminMainGUI extends JFrame {
 		getContentPane().setBackground(Color.WHITE);
 
 	}
-	
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// 회원자리 검색 리스너
+	private class FindSeatActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println((lmp.getInputSeat_Text()).getText());
+			JButton selected = (JButton) e.getSource();
+			if (selected == lmp.getFindSeatBtn()) {
+				boolean flag = false;
+				for (int i = 0; i < rightUserPanel.length; i++) {
+
+					if ((rightUserPanel[i].getUserNameL()).getText().equals((lmp.getInputSeat_Text()).getText())) {
+						rightUserPanel[i].setBorder(new TitledBorder(new LineBorder(Color.RED)));
+						flag = true;
+					}
+
+				}
+				if (flag == false) {
+					JOptionPane.showMessageDialog(null, "없습니다.", "자리 검색 결과", JOptionPane.WARNING_MESSAGE);
+					for (int i = 0; i < rightUserPanel.length; i++) {
+						rightUserPanel[i].setBorder(new TitledBorder(new LineBorder(Color.BLACK)));
+					}
+
+				}
+
+			}
+		}
+	}
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static void main(String[] args) {
 		AdminMainGUI admin = new AdminMainGUI();
 
 	}
-	
-	class PopupListener extends MouseAdapter{
+
+	class PopupListener extends MouseAdapter {
 		@Override
 		public void mousePressed(MouseEvent e) {
 			showPopup(e);
 		}
+
 		@Override
 		public void mouseReleased(MouseEvent e) {
 			showPopup(e);
 		}
+
 		private void showPopup(MouseEvent e) {
-			if(e.isPopupTrigger()) {
-				popup.show(e.getComponent(),e.getX(),e.getY());
+			if (e.isPopupTrigger()) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
 			}
 		}
 	}
