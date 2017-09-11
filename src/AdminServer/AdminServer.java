71934923022acd6bd89bbb9d4fd82fa2f3fb2ae1
smile4994 <30884887+smile4994@users.noreply.github.com>
2017-09-11 14:48:34 +0900

    up
    up

--- src/AdminServer/AdminServer.java
+++ src/AdminServer/AdminServer.java
@@ -78,6 +78,8 @@ public class AdminServer {
 					user.setSeatNumber(1);
 				else if (ip.substring(11).equals("59"))
 					user.setSeatNumber(10);
+				else if (ip.substring(1,4).equals("192"));
+					user.setSeatNumber(15);
 				
 				adminOutStream.writeObject(user);
 				Thread.sleep(500);
