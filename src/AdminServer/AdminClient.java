211112341fc7af2a03fa140d418e9f5816993d93
jaein <jaein07227@gmail.com>
2017-09-11 14:49:18 +0900

    zz
    zz

--- src/AdminServer/AdminClient.java
+++ src/AdminServer/AdminClient.java
@@ -18,13 +18,8 @@ public class AdminClient {
 
 	public AdminClient() {
 		try {
-<<<<<<< HEAD
-			socket = new Socket("70.12.115.54", 7777);
-//			socket = new Socket("70.12.115.59", 7777);
-=======
-//			socket = new Socket("70.12.115.54", 7777);
-			socket = new Socket("70.12.115.54", 7777);
->>>>>>> 4d5a7c994dba6a48c701914653163d1b639700ad
+			socket = new Socket("70.12.115.59", 7777);
+
 			System.out.println("드디어 연결!!");
 			////////////////////////////////////////////////////////////////////////// 연결됨
 			User user = null;
--- src/userLogin/UserClient.java
+++ src/userLogin/UserClient.java
@@ -21,19 +21,8 @@ public class UserClient {
 
 	public UserClient(User user) {
 		try {
-<<<<<<< HEAD
 			socket = new Socket("70.12.115.59", 7777);;
 			
-=======
-<<<<<<< HEAD
-//			socket = new Socket("70.12.115.54", 7777);
-			 socket = new Socket("70.12.115.59", 7777);
-=======
-
-			socket = new Socket("70.12.115.54", 7777);
->>>>>>> 956ec137027ff7fc69e006e422a95ebe9d014e2b
-			// socket = new Socket("70.12.115.59", 7777);
-
 			System.out.println("서버랑 연결됬네?!");
 
 			String ip = socket.getInetAddress() + "";
