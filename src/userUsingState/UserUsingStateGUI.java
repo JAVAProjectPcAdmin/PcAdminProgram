b8bf3fc9b83ea8c075d1791c98b0ff0b6d80d125
blausues <blausues@gmail.com>
2017-09-11 09:01:00 +0900

    .
    .

--- src/userUsingState/UserUsingStateGUI.java
+++ src/userUsingState/UserUsingStateGUI.java
@@ -24,6 +24,7 @@ import com.oracle.jrockit.jfr.UseConstantPool;
 import AdminServer.User;
 import orderFood.OrderGUI;
 import userLogin.UserClient;
+import userLogin.UserLoginGUI;
 
 public class UserUsingStateGUI extends JFrame {
 	private JLabel nameLb, timeLb, moneyLb, talkLb, orderLb, informationLb, customerNumberLb;
@@ -133,6 +134,7 @@ public class UserUsingStateGUI extends JFrame {
 					e1.printStackTrace();
 				}
 				dispose();
+				UserLoginGUI g = new UserLoginGUI();
 			}
 		});
 		informationBt.setBounds(210, 180, 42, 36);
